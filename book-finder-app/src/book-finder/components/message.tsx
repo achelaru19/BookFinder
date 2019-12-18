ddb8474044016ddc7ce6e20cc64abe3fc077e252
Angel <angel.chelaru@gmail.com>
2019-12-18 12:14:42 +0100

    Created messages page with basic implementation

--- book-finder-app/src/book-finder/components/message.tsx
+++ book-finder-app/src/book-finder/components/message.tsx
@@ -9,6 +9,10 @@ interface Props {
 
 class Message extends React.Component<Props> {
 
+state = {
+    userID: 3
+}
+
 constructor(props){
   super(props);
 }
@@ -16,7 +20,13 @@ constructor(props){
   render() {
     return (
       <View style={{flex: 1, flexDirection: 'row', justifyContent: 'space-around', alignItems: 'stretch',}}>
-        <Text> Messaggio: {this.props.message} </Text>
+         {
+            (this.state.userID == this.props.sender) ?
+                <Text>Io: </Text>
+            : 
+                <Text>Altro: </Text> 
+        } 
+        <Text>{this.props.message} </Text>
       </View> 
     );
   }
