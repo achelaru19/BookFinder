478960e8ca0d90d6abd3171401e60408bcf16f53
Angel <angel.chelaru@gmail.com>
2019-12-23 20:24:58 +0100

    Added feature get info from isbn using barcode

--- book-finder-app/src/book-finder/components/barcodeScan.tsx
+++ book-finder-app/src/book-finder/components/barcodeScan.tsx
@@ -1,10 +1,19 @@
 import React, { useState, useEffect } from 'react';
-import { Text, View, StyleSheet, Button } from 'react-native';
-import { BarCodeScanner } from 'expo';
+import { Text, View, StyleSheet, Button, TextInput } from 'react-native';
+import { BarCodeScanner } from 'expo-barcode-scanner';
+import { Icon } from 'react-native-elements';
+import { Formik } from 'formik';
 
-export default function App() {
+export default function BarcodeScan() {
   const [hasPermission, setHasPermission] = useState(null);
   const [scanned, setScanned] = useState(false);
+  const [hasPressedCamera, pressCamera] = useState(false);
+  const [title, setTitle] = useState('');
+  const [author, setAuthor] = useState('');
+  const [editor, setEditor] = useState('');
+  const [isbn, setISBN] = useState('');
+  const [subject, setSubject] = useState('');
+  const [teacher, setTeacher] = useState('');
 
   useEffect(() => {
     (async () => {
@@ -14,8 +23,40 @@ export default function App() {
   }, []);
 
   const handleBarCodeScanned = ({ type, data }) => {
-    setScanned(true);
-    console.log(`Bar code with type ${type} and data ${data} has been scanned!`);
+    if(type == 32 || type == 10) {
+        setScanned(true);
+        // SEARCH FOR ISBN WITH data
+        // "http://openlibrary.org/api/books?bibkeys=ISBN:9780141033570&jscmd=data&format=json"
+        let response = {
+                "publishers": [
+                {
+                    "name": "Penguin Group"
+                }
+                ],
+                "title": "Thinking, fast and slow",
+                "url": "http://openlibrary.org/books/OL25426844M/Thinking_fast_and_slow",
+                "number_of_pages": 512,
+                "cover": {
+                "small": "https://covers.openlibrary.org/b/id/7256677-S.jpg",
+                "large": "https://covers.openlibrary.org/b/id/7256677-L.jpg",
+                "medium": "https://covers.openlibrary.org/b/id/7256677-M.jpg"
+                },
+                "authors": [
+                {
+                    "url": "http://openlibrary.org/authors/OL2066695A/Daniel_Kahneman",
+                    "name": "Daniel Kahneman"
+                }
+                ],
+                "publish_date": "2012"
+            
+        };
+
+        pressCamera(!hasPressedCamera)
+        setTitle(response.title);
+        setAuthor(response.authors[0].name);
+        setEditor(response.publishers[0].name);
+        setISBN(data);
+    }
   };
 
   if (hasPermission === null) {
@@ -28,18 +69,72 @@ export default function App() {
   return (
     <View
       style={{
-        flex: 1,
+        flex: 15,
         flexDirection: 'column',
         justifyContent: 'flex-end',
       }}>
+
+      <View style={{flex:5, flexDirection: 'row'}}>
+        <Text>Ottieni le informazioni con il codice a barre </Text> 
+        <Icon name="camera-alt" reverse={true} onPress={() => pressCamera(!hasPressedCamera)}/>
+      </View>
+
+      <View style={{flex:10}}>
+      {hasPressedCamera ? 
       <BarCodeScanner
         onBarCodeScanned={scanned ? undefined : handleBarCodeScanned}
         style={StyleSheet.absoluteFillObject}
       />
-
-      {scanned && (
-        <Button title={'Tap to Scan Again'} onPress={() => setScanned(false)} />
-      )}
+      : <View style={{flex: 8}}>
+      
+          <View>
+            <View>
+            <Text>Titolo: </Text>
+            <TextInput
+              onChangeText={() => setTitle(this.value)}
+              value={title}
+            />
+            </View>
+            <View>
+              <Text>Autore: </Text>
+            <TextInput
+              onChangeText={() => setAuthor(this.value)}
+              value={author}
+            />
+            </View>
+            <View>
+            <Text>Editore: </Text>
+            <TextInput
+              onChangeText={() => setEditor(this.value)}
+              value={editor}
+            />
+            </View>
+            <View>
+              <Text>ISBN: </Text>
+            <TextInput
+              onChangeText={() => setISBN(this.value)}
+              value={isbn}
+            />
+            </View>
+            <View>
+              <Text>Materia: </Text>
+            <TextInput
+              onChangeText={() => setSubject(this.value)}
+              value={subject}
+            />
+            </View>
+            <View>
+              <Text>Professore: </Text>
+            <TextInput
+              onChangeText={() => setTeacher(this.value)}
+              value={teacher}
+            />
+            </View>
+            <Button onPress={() => console.log("Aggiungi libro")} title="Aggiungi" />
+          </View>
+        </View>
+        }
+        </View>
     </View>
   );
 }
--- book-finder-app/src/book-finder/scenes/addBook.tsx
+++ book-finder-app/src/book-finder/scenes/addBook.tsx
@@ -1,8 +1,6 @@
 import React from 'react';
-import { StyleSheet, Text, View, Button, TextInput, Alert,TouchableOpacity, Image, AppRegistry } from 'react-native';
-import {RNCamera} from 'react-native-camera';
-import { Formik } from 'formik';
-import { Icon } from 'react-native-elements';
+import { StyleSheet, View } from 'react-native';
+
 import NavBar from '../components/navBar';
 import BarcodeScan from '../components/barcodeScan';
 
@@ -28,79 +26,10 @@ export default class AddBook extends React.Component<Props> {
       <View style={{flex: 1}}>
         <NavBar title="Aggiungi un libro"/>
 
-        <View>
-            <Text>Ottieni le informazioni con il codice a barre </Text> 
-            <Icon name="camera-alt" reverse={true} />
+        <View style={{flex: 4}}>
             <BarcodeScan />
         </View>
-        <View style={{flex: 12}}>
-          <Formik
-            initialValues={
-              { title: '',
-                author: '',
-                editor: '',
-                isbn: '',
-                subject: '',
-                teacher: ''
-            }
-            }
-            onSubmit={values => console.log(values)}
-          >
-            {({ handleChange, handleBlur, handleSubmit, values }) => (
-              <View>
-                <View>
-                <Text>Titolo: </Text>
-                <TextInput
-                  onChangeText={handleChange('title')}
-                  onBlur={handleBlur('title')}
-                  value={values.title}
-                />
-                </View>
-                <View>
-                  <Text>Autore: </Text>
-                <TextInput
-                  onChangeText={handleChange('author')}
-                  onBlur={handleBlur('author')}
-                  value={values.author}
-                />
-                </View>
-                <View>
-                <Text>Editore: </Text>
-                <TextInput
-                  onChangeText={handleChange('editor')}
-                  onBlur={handleBlur('editor')}
-                  value={values.editor}
-                />
-                </View>
-                <View>
-                  <Text>ISBN: </Text>
-                <TextInput
-                  onChangeText={handleChange('isbn')}
-                  onBlur={handleBlur('isbn')}
-                  value={values.isbn}
-                />
-                </View>
-                <View>
-                  <Text>Materia: </Text>
-                <TextInput
-                  onChangeText={handleChange('subject')}
-                  onBlur={handleBlur('subject')}
-                  value={values.subject}
-                />
-                </View>
-                <View>
-                  <Text>Professore: </Text>
-                <TextInput
-                  onChangeText={handleChange('teacher')}
-                  onBlur={handleBlur('teacher')}
-                  value={values.teacher}
-                />
-                </View>
-                <Button onPress={() => handleSubmit} title="Aggiungi" />
-              </View>
-            )}
-          </Formik> 
-        </View>
+        
       </View>
     );
   }
