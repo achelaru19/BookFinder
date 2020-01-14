c2595b18004b4e71b816274aec866dac3b984956
Angel <angel.chelaru@gmail.com>
2020-01-14 21:59:40 +0100

    Added firebase cli

--- book-finder-app/src/book-finder/components/message.tsx
+++ book-finder-app/src/book-finder/components/message.tsx
@@ -1,36 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Text, View } from 'react-native';
+import { TouchableOpacity } from 'react-native-gesture-handler';
+import { useNavigation } from 'react-navigation-hooks';
 
 
-interface Props {
-  sender: number,
-  message: string
+export default function Message(props) {
+
+  const [userID, setUserID] = useState(3);
+
+  const navigation = useNavigation();
+
+  return (
+    <View style={{flex: 1, flexDirection: 'row', justifyContent: 'space-around', alignItems: 'stretch',}}>
+      <TouchableOpacity onPress={() => navigation.navigate('Chat', {})}>
+        {
+          (userID == props.sender) ?
+              <Text>Io: </Text>
+          : 
+              <Text>Altro: </Text> 
+      } 
+      <Text>{props.message} </Text>
+      </TouchableOpacity>
+    </View> 
+  );
+  
 }
-
-class Message extends React.Component<Props> {
-
-state = {
-    userID: 3
-}
-
-constructor(props){
-  super(props);
-}
-
-  render() {
-    return (
-      <View style={{flex: 1, flexDirection: 'row', justifyContent: 'space-around', alignItems: 'stretch',}}>
-         {
-            (this.state.userID == this.props.sender) ?
-                <Text>Io: </Text>
-            : 
-                <Text>Altro: </Text> 
-        } 
-        <Text>{this.props.message} </Text>
-      </View> 
-    );
-  }
-}
-
-
-export default Message;
