381bc7e52fd9b044c31555f84a814c4a9834472e
Angel <angel.chelaru@gmail.com>
2020-05-21 18:32:00 +0200

    Added splash image

--- book-finder-app/functions/src/index.tsx
+++ book-finder-app/functions/src/index.tsx
@@ -1,31 +1,93 @@
 import * as functions from 'firebase-functions';
 
 
-
 const admin = require('firebase-admin');
-
- export const helloWorld = functions.https.onRequest((request, response) => {
-  response.send("Hello from Firebase!");
- });
-
-export const confirmUser = functions.auth.user().onCreate((user) => {
-    const docRef = admin.database.collection('users').doc(user.email);
-
-    docRef.set({
-        validated: true
-    });
-});
+admin.initializeApp(functions.config().firebase);
+const db = admin.firestore();
 
 
-exports.createUser = functions.firestore
+exports.sendPushNotifications = functions.firestore
     .document('books/{bookID}')
     .onCreate((snap, context) => {
       // Get an object representing the document
-      // e.g. {'name': 'Marie', 'age': 66}
-      //const newValue = snap.data();
+      // e.g. {'title': 'Business 101', 'author': 'Micheal Scott', ...}
+      const newValue = snap.data();
 
       // access a particular field as you would any JS property
-      //const name = newValue.name;
+      const title = newValue.title;
+      const author = newValue.author;
+      const isbn = newValue.isbn;
+      const university = newValue.sellerUniversity;
 
-      // perform desired operations ...
-    });
+      const emails = getUsersEmails(title, author, isbn);
+      const tokens = getTokens(emails, university);
+
+      const payload = {
+          notification: {
+              title: 'Good news!',
+              body: 'Someone added a book that was in your WishList!'
+          }
+      };
+
+      admin.messaging().sendToDevice(tokens, payload); 
+});
+
+
+async function getTokens(emails, university){
+    const tokens = [];
+    await emails.forEach(email => {
+         db.collection('tokens')
+        .where('email', '==', email)
+        .limit(1)
+        .get()
+        .then(snapshot => {
+            snapshot.forEach(doc => {
+                const tokenData = doc.data()
+                if(tokenData.university === university){
+                    tokens.push(tokenData.token);
+                }
+            })
+        })
+    })
+    return tokens;
+}
+
+async function getUsersEmails(title, author, isbn) {
+    const emails = new Set();
+    await db.collection('wishlist')
+    .where('title', '==', title)
+    .get()
+    .then(snapshot => {
+        snapshot.forEach(doc => {
+            const bookData = doc.data()
+            if(!emails.has(bookData.user)){
+                emails.add(bookData.user);
+            }
+        })
+    })
+
+    await db.collection('wishlist')
+    .where('author', '==', author)
+    .get()
+    .then(snapshot => {
+        snapshot.forEach(doc => {
+            const bookData = doc.data()
+            if(!emails.has(bookData.user)){
+                emails.add(bookData.user);
+            }
+        })
+    })
+
+    await db.collection('wishlist')
+    .where('isbn', '==', isbn)
+    .get()
+    .then(snapshot => {
+        snapshot.forEach(doc => {
+            const bookData = doc.data()
+            if(!emails.has(bookData.user)){
+                emails.add(bookData.user);
+            }
+        })
+    })
+    return Array.from(emails);
+}
