c0196e1f95a2064981d0b976ae2644e4fb23496b
Angel <angel.chelaru@gmail.com>
2020-06-11 13:50:11 +0200

    removed old packages

--- serverless_lambda_functions/add_to_wishlist/rds_config.py
+++
@@ -1,5 +0,0 @@
-#config file containing credentials for RDS MySQL instance
-db_username = "achelaru"
-db_password = "Cosimini28Marruota"
-db_name = "bookfinder" 
-
--- serverless_lambda_functions/lambda_fun/rds_config.py
+++
@@ -1,4 +0,0 @@
-#config file containing credentials for RDS MySQL instance
-db_username = "a_chelaru19"
-db_password = "Cosimini28Marruota"
-db_name = "book-finder" 
